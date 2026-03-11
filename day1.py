b057811eadbfb5a2e3d6993e688dc34791fbb144
xwinstonlava-hash <xwinstonlava@gmail.com>
2026-03-11 18:29:37 +0530

    Day 1 Python Practice

--- day1.py
+++ day1.py
@@ -1,43 +1,47 @@
-# lst = [1,2,3,4,5]
-# Total = sum(lst)
-# print(Total) 
+# Write a Python program to calculate the sum of all numbers in a list
+lst = [1,2,3,4,5]
+Total = sum(lst)
+print(Total) 
 
-# numbers = [10,40,50,20,60]
-# maximum = numbers[0]
-# for num in numbers:
-#     if num > maximum:
-#         maximum = num 
-# print("Maximum number:", maximum)    
+# Find the largest number in a list without using max().
 
-# numbers = list(map(int,input("Enter numbers:").split()))
-# even = 0
-# odd  = 0
+numbers = [10,40,50,20,60]
+maximum = numbers[0]
+for num in numbers:
+    if num > maximum:
+        maximum = num 
+print("Maximum number:", maximum)  
 
-# for num in numbers:
-#     if num%2==0:
-#         even+=1
-#     else:
-#         odd+=1
+# Count how many even and odd numbers are present in a list.
 
-# print("Total even numbers:" , even)
-# print("Total odd numbers:",odd)            
+numbers = list(map(int,input("Enter numbers:").split()))
+even = 0
+odd  = 0
 
+for num in numbers:
+    if num%2==0:
+        even+=1
+    else:
+        odd+=1
 
-
-# numbers = [10,40,50,20,60]
-# start = 0
-# end = len(numbers)-1
-# while start<end:
-#     numbers[start] , numbers[end] = numbers[end] , numbers[start]
-
-#     start+=1
-#     end-=1
-# print("Reversed List:" , numbers)    
+print("Total even numbers:" , even)
+print("Total odd numbers:",odd)            
 
 
+# Reverse a list without using reverse() or slicing [::-1].
+
+numbers = [10,40,50,20,60]
+start = 0
+end = len(numbers)-1
+while start<end:
+    numbers[start] , numbers[end] = numbers[end] , numbers[start]
+    start+=1
+    end-=1
+print("Reversed List:" , numbers)    
+
+# Remove duplicate elements from a list.
 
 numbers = [1,1,2,3,2,4,5,5,5,5,6,6,7,7,7,]
-
 unique =[]
 for num in numbers:
     if num not in unique:
