42ae4e8999faa67c4c6199732a7aaabe9b5351ba
Shivam Dwivedi <shivamdwivedicse20919@gmail.com>
2026-04-01 22:35:03 +0530

    Added practice question of day 23

---
+++ day22.py
@@ -0,0 +1,9 @@
+result = input("Enter your result (pass/fail): ")
+
+if result.lower() == "pass":
+    print("Education is the key to success.")
+else:
+    print("A single sheet of paper cannot decide my future.")
+
+
+
---
+++ day23.py
@@ -0,0 +1,19 @@
+
+import random
+# Generate random number between 1 and 100
+number = random.randint(1, 100)
+attempts = 0
+
+print("Guess the number between 1 and 100")
+while True:
+    guess = int(input("Enter your guess: "))
+    attempts += 1
+    if guess < number:
+        print("Too low")
+    elif guess > number:
+        print("Too high")
+    else:
+        print("Correct 🎉")
+        print("Attempts:", attempts)
+        break
+
